46c4ee6bfff92de2074ec9d4f63b0f2f75747754
misscreeptoe <misscreeptoe@pm.me>
2022-12-18 15:55:24 +0100

    feat: add dashboard component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,18 +1,24 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
       declarations: [AppComponent],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
   });
 
   it('should create', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    fixture.detectChanges();
+
+    expect(component).toBeTruthy();
   });
 });
---
+++ src/app/ui/container/container.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-container',
+  templateUrl: './container.component.html',
+  styleUrls: ['./container.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ContainerComponent {}
